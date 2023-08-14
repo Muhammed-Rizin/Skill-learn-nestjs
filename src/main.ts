4504192b43ca4ce3bfd35824aa574a127863bdec
Muhammed-Rizin <Rizin7427@gmail.com>
2023-08-14 16:19:42 +0530

    cors changed for new domain

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://skilllearn.netlify.app/home'],
     credentials: true,
   };
   app.enableCors(corsOptions);
