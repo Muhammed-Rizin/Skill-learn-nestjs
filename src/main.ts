a66dd3d1d51fc207e5272945dc8542ce57d4217a
Muhammed-Rizin <rizin7427@gmail.com>
2024-10-18 20:25:08 +0530

    Morgan

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import * as dotenv from 'dotenv';
+import * as morgan from 'morgan';
 dotenv.config();
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'));
+
   const corsOptions: CorsOptions = {
     origin: allowedOrigins,
     credentials: true,
